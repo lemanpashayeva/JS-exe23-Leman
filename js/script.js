08cb24bffe236cf58b6f3f88ae12209e7407c6df
lemanpashayeva <lmnpasayeva@gmail.com>
2022-10-29 12:27:05 +0400

    last commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 'use strict';
 
+const btn = document.getElementById('btn');
+const names = document.getElementById('ad');
+const array = [];
+
+btn.addEventListener('click' , function() {
+    let n = array.findIndex(function (value) {
+        return names.value==value;
+    });
+    
+    if (n==-1) {
+        array.push(names.value);
+    } else {
+        window.alert('bu ad artiq daxil edilib');
+    }
+    
+    
+    console.log(array);
+});
+
+
 //second exercise
 
-
+/*
 const arr = [5, 2, 4, 1, 15, 8, 3];
 
 const arr1 = arr.map(function(value){
@@ -19,10 +39,10 @@ const arr2 = arr1.reduce(function(acc, value){
 
 console.log(arr);
 console.log(arr1);
-console.log(arr2/2);
+console.log(arr2/2); 
 
 
-/*
+
 const arr = [16, 6, 10, 5, 6, 1, 4];
 
 const arr1 = arr.map(function(value){
